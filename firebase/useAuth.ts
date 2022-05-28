e1cd58a031ec7bf8d4f48d17817aeadf284a19f3
staaph <contact.staaph@gmail.com>
2022-05-28 14:23:53 +0200

    refactor

--- firebase/useAuth.ts
+++ firebase/useAuth.ts
@@ -13,7 +13,6 @@ import { FirebaseError } from '@firebase/util';
 
 export const errorMsg: Ref<string | unknown> = ref();
 
-<<<<<<< HEAD
 /**
  * login with email & password
  * @param email pass email from input
@@ -100,102 +99,4 @@ export const logout = async () => {
       errorMsg.value = 'unknown server error';
     }
   }
-=======
-  /**
-   * login with email & password
-   * @param email - pass email from input
-   * @param password - pass password from input
-   */
-  const login = async (email: string, password: string) => {
-    errorMsg.value = '';
-    try {
-      await signInWithEmailAndPassword(getAuth(), email, password);
-    } catch (error: unknown) {
-      if (error instanceof FirebaseError) {
-        const errorMessageMap: { [key: string]: string } = {
-          'auth/invalid-email': 'Invalid email',
-          'auth/wrong-password': 'Incorrect password',
-          'auth/user-not-found': 'No account with the provided email found',
-        };
-        errorMsg.value = errorMessageMap[error.code] ?? 'Incorrect credentials';
-      } else {
-        errorMsg.value = 'unknown server error';
-      }
-    }
-  };
-
-  /**
-   * sign user in using email and password
-   * @param email - pass email from input
-   * @param password - pass password from input
-   */
-  const signup = async (email: string, password: string) => {
-    errorMsg.value = '';
-    try {
-      await createUserWithEmailAndPassword(getAuth(), email, password);
-    } catch (error: unknown) {
-      if (error instanceof FirebaseError) {
-        const errorMessageMap: { [key: string]: string } = {
-          'auth/weak-password': 'password must contain at least 6 characters',
-          'auth/email-already-in-use': 'email already taken',
-        };
-        errorMsg.value =
-          errorMessageMap[error.code] ?? 'Something unexpected happened';
-      }
-    }
-  };
-
-  /**
-   * log user in anonymously
-   */
-  const loginAnonymous = () => {
-    errorMsg.value = '';
-    try {
-      signInAnonymously(getAuth());
-    } catch (error: unknown) {
-      if (error instanceof FirebaseError) {
-        errorMsg.value = 'Something unexpected happened';
-      } else {
-        errorMsg.value = 'unknown server error';
-      }
-    }
-  };
-
-  /**
-   * login with Google, opens popup
-   */
-  const loginWithGoogle = async () => {
-    errorMsg.value = '';
-    try {
-      await signInWithPopup(getAuth(), new GoogleAuthProvider());
-    } catch (error) {
-      errorMsg.value = 'Something unexpected happened';
-    }
-  };
-
-  /**
-   * log user out
-   */
-  const logout = async () => {
-    errorMsg.value = '';
-    try {
-      await signOut(getAuth());
-    } catch (error: unknown) {
-      if (error instanceof FirebaseError) {
-        errorMsg.value = 'Something unexpected happened.';
-      } else {
-        errorMsg.value = 'unknown server error';
-      }
-    }
-  };
-
-  return {
-    login,
-    signup,
-    loginAnonymous,
-    loginWithGoogle,
-    logout,
-    errorMsg,
-  };
->>>>>>> refs/remotes/origin/master
 };
--- firebase/useFirestore.ts
+++ firebase/useFirestore.ts
@@ -9,83 +9,22 @@ import {
   updateDoc,
 } from 'firebase/firestore';
 
-<<<<<<< HEAD
-/**
- * add document to firestore
- * @param reference database reference
- * @param data add data as Object
- */
-export const addDocument = async (reference: string, data: object) => {
-  await addDoc(collection(getFirestore(), reference), data);
-};
-
-/**
- * get documents by collection
- * @param reference database reference
- */
-export const getDocument = async (reference: string) => {
-  const documents: Array<object> = [];
-  const snapshot = await getDocs(collection(getFirestore(), reference));
-  snapshot.forEach((doc) => documents.push({ ...doc.data() }));
-  return documents;
-};
-
-/**
- * set documents with naming and merging option
- * @param reference database reference
- * @param docname name of the document
- * @param data pass data as Object
- * @param options merge with existing data or enable overwriting ?
- */
-export const setDocument = async (
-  reference: string,
-  docname: string,
-  data: object,
-  options: { merge?: boolean } = { merge: false }
-) => {
-  await setDoc(doc(getFirestore(), reference, docname), data, options);
-};
-
-/**
- * delete single document in collection
- * @param reference database reference
- * @param document pass document name or ID
- */
-export const deleteDocument = async (reference: string, document: string) => {
-  await deleteDoc(doc(getFirestore(), reference, document));
-};
-
-/**
- * update a document by name or id
- * @param reference database reference
- * @param document pass Document Name or ID
- * @param data add data in Object format
- *
- */
-export const updateDocument = async (
-  reference: string,
-  document: string,
-  data: object
-) => {
-  await updateDoc(doc(getFirestore(), reference, document), data);
-=======
-export const useFirestore = () => {
 
   /**
    * add document to firestore
-   * @param reference - database reference
-   * @param data add - data as Object
+   * @param reference database reference
+   * @param data add data as Object
    */
-  const addDocument = async (reference: string, data: object) => {
+  export const addDocument = async (reference: string, data: object) => {
     await addDoc(collection(getFirestore(), reference), data);
   };
 
 
   /**
    * get documents by collection
-   * @param reference - database reference
+   * @param reference database reference
    */
-  const getDocument = async (reference: string) => {
+  export const getDocument = async (reference: string) => {
     const documents: Array<object> = [];
     const snapshot = await getDocs(collection(getFirestore(), reference));
     snapshot.forEach((doc) => documents.push({ ...doc.data() }));
@@ -94,12 +33,12 @@ export const useFirestore = () => {
 
   /**
    * set documents with naming and merging option
-   * @param reference - database reference
-   * @param docname - name of the document
-   * @param data - pass data as Object
-   * @param options - merge with existing data or enable overwriting ?
+   * @param reference database reference
+   * @param docname name of the document
+   * @param data pass data as Object
+   * @param options merge with existing data or enable overwriting ?
    */
-  const setDocument = async (
+  export const setDocument = async (
     reference: string,
     docname: string,
     data: object,
@@ -110,34 +49,24 @@ export const useFirestore = () => {
 
   /**
    * delete single document in collection
-   * @param reference - database reference
-   * @param document - pass document name or ID
+   * @param reference database reference
+   * @param document pass document name or ID
    */
-  const deleteDocument = async (reference: string, document: string) => {
+  export const deleteDocument = async (reference: string, document: string) => {
     await deleteDoc(doc(getFirestore(), reference, document));
   };
 
   /**
    * update a document by name or id
-   * @param reference - database reference
-   * @param document - pass Document Name or ID
-   * @param data - add data in Object format
+   * @param reference database reference
+   * @param document pass Document Name or ID
+   * @param data add data in Object format
    * 
    */
-  const updateDocument = async (
+  export const updateDocument = async (
     reference: string,
     document: string,
     data: object
   ) => {
     await updateDoc(doc(getFirestore(), reference, document), data);
   };
-
-  return {
-    addDocument,
-    getDocument,
-    setDocument,
-    deleteDocument,
-    updateDocument,
-  };
->>>>>>> refs/remotes/origin/master
-};
